0b91b0e8fd5d18830f69783e74d82e8e76bafab6
AyemDanm <danielMMomodu@gmail.com>
2024-10-28 12:27:49 +0100

    concat2

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 """
 This module contains a function to concatenate two strings.
 
@@ -9,5 +8,14 @@ Functions:
         Concatenates two strings and returns the result.
 """
 
+
 def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings.
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+    Returns:
+        str: The concatenated result of str1 and str2.
+    """
     return str1 + str2
